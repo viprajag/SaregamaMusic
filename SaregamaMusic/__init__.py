f5a6cf742cc40006a7f8ca9b773c01ffc52e50b7
saregamamusic <bittulkr628@gmail.com>
2025-04-13 21:07:13 +0530

    SaregamaMusic

--- SaregamaMusic/__init__.py
+++ SaregamaMusic/__init__.py
@@ -1,1 +1,27 @@
+from SaregamaMusic.core.bot import AMBOTOP
+from SaregamaMusic.core.dir import dirr
+from SaregamaMusic.core.git import git
+from SaregamaMusic.core.userbot import Userbot
+from SaregamaMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = AMBOTOP()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
 
