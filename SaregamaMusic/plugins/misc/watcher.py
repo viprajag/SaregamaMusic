99173574b7b6383336dd627ca11b25eed37fdc16
saregamamusic <bittulkr628@gmail.com>
2025-04-14 17:50:25 +0530

    saregamaMusic

--- SaregamaMusic/plugins/misc/watcher.py
+++ SaregamaMusic/plugins/misc/watcher.py
@@ -1,1 +1,15 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from SaregamaMusic import app
+from SaregamaMusic.core.call import AMBOTOP
+
+welcome = 20
+close = 30
+
+
+@app.on_message(filters.video_chat_started, group=welcome)
+@app.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await AMBOTOP.stop_stream_force(message.chat.id)
 
