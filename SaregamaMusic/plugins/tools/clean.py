f7dbefebb121a43dd0347087d94c9c5007e8f9fd
saregamamusic <bittulkr628@gmail.com>
2025-04-14 19:59:10 +0530

    saregamaMusic

--- SaregamaMusic/plugins/tools/clean.py
+++ SaregamaMusic/plugins/tools/clean.py
@@ -1,1 +1,18 @@
+import os
+import shutil
+from pyrogram import filters
+from SaregamaMusic import app
+from SaregamaMusic.misc import SUDOERS
+
+
+@app.on_message(filters.command("clean") & SUDOERS)
+async def clean(client, message):
+    A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
+    dir = "downloads"
+    dir1 = "cache"
+    shutil.rmtree(dir)
+    shutil.rmtree(dir1)
+    os.mkdir(dir)
+    os.mkdir(dir1)
+    await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
 
