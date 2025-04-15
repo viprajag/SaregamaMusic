abe59a84d19f1d3a94db3351196c9ad11fd4cb85
saregamamusic <bittulkr628@gmail.com>
2025-04-15 08:25:40 +0530

    Update Soundcloud.py

--- SaregamaMusic/platforms/Soundcloud.py
+++ SaregamaMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from RessoMusic.utils.formatters import seconds_to_min
+from SaregamaMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
