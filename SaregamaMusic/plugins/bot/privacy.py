0102934e97dbf056190b2f59e46d4cbee89b9eb2
Kittumusic <rajababutg825@gmail.com>
2025-08-26 07:55:12 +0530

    Update privacy.py

--- SaregamaMusic/plugins/bot/privacy.py
+++ SaregamaMusic/plugins/bot/privacy.py
@@ -9,7 +9,7 @@ TEXT = f"""
 
 Your privacy is important to us. To learn more about how we collect, use, and protect your data, please review our Privacy Policy here: [Privacy Policy]({config.PRIVACY_LINK}). 
 
-If you have any questions or concerns, feel free to reach out to our [support team](https://t.me/Music_World_Support).
+If you have any questions or concerns, feel free to reach out to our [support team](https://t.me/Friend_Ki_Masti).
 """
 
 @app.on_message(filters.command("privacy"))
