4088cb1bc9534e2710e7b38065e14fd44ecf7e8c
Nysamusic <rajatech684@gmail.com>
2025-09-26 10:41:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,8 +22,8 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
 
 # Vars For API End Pont.
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", "NxGBNexGenBotsee3692") # youtube song api key, get it from https://t.me/MR_ROCKY_TZ
+BASE_API_URL = "https://zyro.zyronetworks.shop"
+BASE_API_KEY = "IcDU1vq1WSMo5XfpatPmsMNanB5eRkM1"
 
 # Get this value from @MissRose_Bot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", None)) 
