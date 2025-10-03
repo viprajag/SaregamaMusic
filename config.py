a77bca62e0fbc87f004173a61cf2d77e6ac1d25f
viprajag <viprajatg@gmail.com>
2025-10-03 12:49:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Nysamusic/SaregamaMusic",
+    "https://github.com/viprajag/SaregamaMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
